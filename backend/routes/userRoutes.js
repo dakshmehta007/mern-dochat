5dda10a0618c4e63fd55b642cb462ad06423835d
dakshmehta007 <mehtadaksh85@gmail.com>
2025-08-03 20:47:13 +0530

    Initial commite

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,8 +6,6 @@ const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.route('/').post(registerUser).get(protect ,allUser);
-
-
 router.post('/login',authUser);
 
 module.exports = router;
